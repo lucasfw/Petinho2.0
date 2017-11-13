6c069a59645073d18511feab2aaa1558aab6cb40
igorasr18 <30876681+igorasr18@users.noreply.github.com>
2017-11-13 09:38:04 -0300

    adicionando tutorial

---
+++ app/src/main/java/com/example/lucascarvalho/adopet/Apresentacao2.java
@@ -0,0 +1,26 @@
+package com.example.lucascarvalho.adopet;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Apresentacao2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apresentacao2);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                startActivity(new Intent(getBaseContext(), Apresentacao3.class));
+                finish();
+            }
+        },5000);
+    }
+}
